04a57ba53e6a3a29d54d0c510f1150b70b1497f7
Brian Kwak <briankwak49@gmail.com>
2017-11-14 20:38:09 -0800

    Writing documentation for classes
    If anyone is reading this, AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAHHHHHHHHHHH
    I have no idea what I'm doing, I just want an A in this class ;_;

--- LunchMeet/app/src/main/java/com/example/lunchmeet/lunchmeet/DBManager.java
+++ LunchMeet/app/src/main/java/com/example/lunchmeet/lunchmeet/DBManager.java
@@ -18,34 +18,69 @@ import java.util.Map;
  * Created by Brian Kwak on 11/4/2017.
  */
 
+/**
+ * The sole point of communication between the rest of the app and Firebase's realtime database.
+ *
+ * @author Brian Kwak
+ */
 public class DBManager{
     private static final DBManager instance = new DBManager();
     private DatabaseReference database;
     private String TAG = "DBManager";
 
-
     private DBManager(){
         database = FirebaseDatabase.getInstance().getReference();
     }
 
+    /**
+     * The getter method to return the single instance of the class, as per the Singleton pattern.
+     * The instance is automatically created via eager instantiation.
+     * Returns the single instance of the DBManager class.
+     *
+     * @return The single instance of the DBManager class.
+     */
     static DBManager getInstance(){
         return instance;
     }
 
+    /**
+     * Adds a user to the database.
+     *
+     * @param u The user to be added to the database.
+     */
     void addUser(DBUser u){
         database.child("users").child(u.getUid()).setValue(u.toMap());
     }
-    void addUser(String uid, String name, String photoUrl){
-        DatabaseReference ref = database.child("users").child(uid);
-        ref.child("name").setValue(name);
-        ref.child("photoUrl").setValue(photoUrl);
-    }
+
+    /**
+     * Removes a user from the database.
+     *
+     * @param u The user to be removed from the database.
+     */
     void removeUser(DBUser u){
         removeUser(u.getUid());
     }
+
+    /**
+     * Removes a user from the database, using the UID only.
+     *
+     * @param uid The UID of the user to be removed from the database.
+     */
     void removeUser(String uid){
         database.child("users").child(uid).removeValue();
     }
+
+    /**
+     * Retrieves user information corresponding to the given UID from the database.
+     * Database information is read asynchronously, so a listener is required
+     * to call the proper methods after the read is sucessful.
+     *
+     * In this case, the user information is collected as a DBUser which is sent to the
+     * provided DBListener, which can then run based off that DBUser.
+     *
+     * @param o The DBListener that will process the user information read from the database.
+     * @param id The user ID of the user whose information should be read from the database.
+     */
     void getUserFromUid(DBListener<DBUser> o, String id){
         final DBListener obs = o;
         final String uid = id;
@@ -64,17 +99,50 @@ public class DBManager{
         });
     }
 
+    /**
+     * Updates an active user's location in the database.
+     *
+     * @param u The user whose location should be updated.
+     * @param lat The latitude of the user.
+     * @param lng The longitude of the user.
+     */
     void updateActiveUser(DBUser u, double lat, double lng){
         updateActiveUser(u.getUid(),lat,lng);
     }
+
+    /**
+     * Updates an active user's location in the database based on the user's UID.
+     *
+     * @param uid The ID of the user whose location should be updated.
+     * @param lat The latitude of the user.
+     * @param lng The longitude of the user.
+     */
     void updateActiveUser(String uid, double lat, double lng){
         DatabaseReference ref = database.child("active").child(uid);
         ref.child("lat").setValue(lat);
         ref.child("lng").setValue(lng);
     }
+
+    /**
+     * Removes a user from the active users section of the database. To be used when a user goes
+     * inactive (e.g. closes the app).
+     *
+     * @param uid The ID of the user to be removed.
+     */
     void removeActiveUser(String uid){
         database.child("active").child(uid).removeValue();
     }
+
+    /**
+     * Attaches a listener to the database to read data about active users from the database.
+     * When data about active users is read, the data will be presented as a List of DBActives,
+     * which can then be processed by the provided DBListener.
+     *
+     * This method needs to be called only once, and the DBListener will run its run() method
+     * every time the list of active users updates.
+     *
+     * @param o The DBListener that will process the active user information from the database.
+     */
     void attachListenerForActiveUsers(DBListener<List<DBActive>> o){
         final DBListener obs = o;
         database.child("active").addValueEventListener(new ValueEventListener(){
@@ -97,21 +165,59 @@ public class DBManager{
         });
     }
 
+    /**
+     * Using the ID of a group, updates the group with the ID of the leader in the database.
+     *
+     * @param gid The ID of the group to be updated.
+     * @param uid The ID of the leader to be updated.
+     */
     void updateGroupLeader(String gid, String uid){
         database.child("groups").child(gid).child("leader").setValue(uid);
     }
+
+    /**
+     * Updates the location of a group in the database.
+     *
+     * @param gid The ID of the group to be updated.
+     * @param lat The latitutde of the group.
+     * @param lng The longitude of the group.
+     */
     void updateGroupLocation(String gid, double lat, double lng){
         DatabaseReference ref = database.child("groups").child(gid);
         ref.child("lat").setValue(lat);
         ref.child("lng").setValue(lng);
 
     }
+
+    /**
+     * Updates the size of a group in the database.
+     *
+     * @param gid The ID of the group to be updated.
+     * @param size The size of the group.
+     */
     void updateGroupSize(String gid, int size){
         database.child("groups").child(gid).child("size").setValue(size);
     }
+
+    /**
+     * Removes a group from the database.
+     *
+     * @param gid The ID of the group to be removed.
+     */
     void removeGroup(String gid){
         database.child("groups").child(gid).removeValue();
     }
+
+    /**
+     * Attaches a listener to the database to read data about groups from the database.
+     * When data about groups is read, the data will be presented as a List of DBGroups,
+     * which can then be processed by the provided DBListener.
+     *
+     * This method needs to be called only once, and the DBListener will run its run() method
+     * every time the list of active users updates.
+     *
+     * @param o The DBListener that will process the group information from the database.
+     */
     void attachListenerForGroups(DBListener<List<DBGroup>> o){
         final DBListener obs = o;
         database.child("groups").addValueEventListener(new ValueEventListener(){
@@ -134,6 +240,12 @@ public class DBManager{
         });
     }
 
+    /**
+     * Adds an entry in the database pertaining to a new group.
+     *
+     * @param uid The ID of the leader that is creating the group. Every group requires a leader.
+     * @return The ID of the group that has just been created.
+     */
     String createGroup(String uid){
         DatabaseReference ref = database.child("members").push();
         String key = ref.getKey();
@@ -141,10 +253,28 @@ public class DBManager{
 
         return key;
     }
+
+    /**
+     * Adds a new member to the list of members in a group.
+     * Also updates the size of the group with the given size.
+     *
+     * @param gid The ID of the group being joined.
+     * @param uid The ID of the user that is joining the group.
+     * @param size The current size of the group.
+     */
     void joinGroup(String gid, String uid, int size){
         database.child("members").child(gid).child(uid).setValue(true);
         database.child("groups").child(gid).child("size").setValue(size + 1);
     }
+
+    /**
+     * Retrieves a list of members in the group corresponding to the given group ID.
+     * When data about the members is read, the data will be presented as a List of
+     * Strings (user IDs) that will then be passed to the DBListener to process.
+     *
+     * @param o The DBListener that will process the member information from the database.
+     * @param gid The ID of the group whose members are being retrieved.
+     */
     void getMembers(DBListener<List<String>> o, String gid){
         final DBListener obs = o;
         database.child("members").child(gid).addListenerForSingleValueEvent(new ValueEventListener(){
