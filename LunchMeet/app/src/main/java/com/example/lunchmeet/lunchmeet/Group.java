a5eedb5dba0e04fb50b7944db72147b7ca25b810
Stephanie Lam <sytl@yahoo.com>
2017-11-15 20:39:45 -0800

    add javadoc for Group and User, add getters + setters for Group

--- LunchMeet/app/src/main/java/com/example/lunchmeet/lunchmeet/Group.java
+++ LunchMeet/app/src/main/java/com/example/lunchmeet/lunchmeet/Group.java
@@ -1,45 +1,141 @@
 package com.example.lunchmeet.lunchmeet;
 
-import java.lang.reflect.Array;
-
 /**
- * Created by NewYeah on 11/4/2017.
+ * A class to keep track of formed lunch group information, such as the members, capacity, and current size
+ * of a group. Includes methods to add and remove members from a group, as well as getters and setters
+ * for the group information.
  */
 
 public class Group {
     private User[] members;
     private int capacity;
     private int curr_size;
-    private float lat,lon;
+    private double lat,lon;
 
+    /**
+     * Default constructor that creates a group of maximum 6 members.
+     * @param user the creator of the group
+     */
     public Group(User user){
-        members=new User[6];
-        members[0]=user;
-        capacity=5;
-        curr_size=1;
-        lat=0;
-        lon=0;
+        members = new User[6]; // default capacity of 6 group members
+        members[0] = user;
+        capacity = 6;
+        curr_size = 1;
+        lat = user.getLat();
+        lon = user.getLong();
     }
 
-    public void add_user(User user){
-        for(int i=0;i<Array.getLength(members);i++){
-            if(members[i]==null){
-                members[i]=user;
+    /**
+     * Parametrized constructor that creates a group with capacity set by the group creator.
+     * @param user the creator of the group
+     * @param c the max capacity of the group
+     */
+    public Group(User user, int c) {
+        members = new User[c]; // capacity set by parameter c
+        members[0] = user;
+        capacity = c;
+        curr_size = 1;
+        lat = user.getLat();
+        lon = user.getLong();
+    }
+
+    /**
+     * Adds a user to the group, if there is space.
+     * @param user the user to add to the group
+     * @return true if the user was added, false if the user was not (user in another group or group reached max capacity)
+     */
+    public boolean add_user(User user){
+        if (curr_size >= capacity)
+            return false;
+
+        members[curr_size] = user;
+        curr_size++;
+        return true;
+    }
+
+    /**
+     * Removes a user from the group.
+     * @param user the user to be removed
+     * @return true if the user was removed, false if the user was not.
+     */
+    public boolean remove_user(User user){
+        for(int i = 0; i < curr_size; i++){
+            if(members[i] == user){
+                members[i] = null;
+                curr_size--;
+                return true;
             }
         }
-
-
+        return false;
     }
 
-    public void remove_user(User user){
-        for(int i=0;i< Array.getLength(members);i++){
-            if(members[i]==user){
-                    members[i]=null;
-            }
-
+    /**
+     * Disbands a group by setting all members to the free agent state.
+     * @return true if the group was successfully disbanded, false otherwise.
+     */
+    public boolean dissolveGroup() {
+        for (int i = 0; i < curr_size; i++) {
+            members[i].setState(members[i].getState("freeagent")); // set all members to free agent state
         }
+        return true;
     }
 
+    /**
+     * Gets the members of the group.
+     * @return array of Users who are members of the group
+     */
+    public User[] getGroupMembers() {
+        return members;
+    }
 
+    /**
+     * Gets the group's maximum capacity.
+     * @return group's maximum capacity
+     */
+    public int getCapacity() {
+        return capacity;
+    }
+
+    /**
+     * Sets the group's max capacity to the parameter c.
+     * @param c new max capacity
+     */
+    public void setCapacity (int c) {
+        capacity = c;
+    }
+
+    /**
+     * Gets the group's current size.
+     * @return number of members currently in the group
+     */
+    public int getCurrentSize() {
+        return curr_size;
+    }
+
+    /**
+     * Sets the group's location to the latitude and longitude specified by the parameters.
+     * @param latitude new group location latitude
+     * @param longitude new group location longitude
+     */
+    public void setGroupLoc(double latitude, double longitude) {
+        lat = latitude;
+        lon = longitude;
+    }
+
+    /**
+     * Gets the group's current latitude.
+     * @return group's latitude
+     */
+    public double getGroupLat() {
+        return lat;
+    }
+
+    /**
+     * Gets the group's current longitude.
+     * @return group's longitude
+     */
+    public double getGroupLong() {
+        return lon;
+    }
 
 }
--- LunchMeet/app/src/main/java/com/example/lunchmeet/lunchmeet/User.java
+++ LunchMeet/app/src/main/java/com/example/lunchmeet/lunchmeet/User.java
@@ -1,10 +1,12 @@
 package com.example.lunchmeet.lunchmeet;
 
 /**
- * Created by NewYeah on 11/4/2017.
+ * A class to keep track of user information, such as their name, gender, and profile picture as taken from Facebook.
+ * Keeps track of the user's current state and allows or disallows certain actions based on the state. Provides
+ * getters and setters to manipulate user information.
  */
-
 public class User {
+
     private String name;
     private String gender;
     private String profile_pic;
@@ -16,6 +18,14 @@ public class User {
     private Member member;
     private Creator creator;
 
+    /**
+     * Default constructor to initialize user information.
+     * @param name user's name
+     * @param gender user's gender
+     * @param profile_pic user's profile picture URL from Facebook
+     * @param lat user's current latitude
+     * @param lon user's current longitude
+     */
     public User(String name,String gender,String profile_pic,float lat,float lon){
         free_agent=new Free_agent();
         member=new Member();
@@ -27,14 +37,28 @@ public class User {
         this.lon=lon;
         this.state=free_agent;
     }
-    public String getName(){
+
+    /**
+     * Gets the user's name.
+     * @return user's name
+     */
+    public String getName() {
         return name;
     }
 
-    public void setState(State s){
-        state=s;
+    /**
+     * Sets the user's current state according to the parameter
+     * @param s state to transition to
+     */
+    public void setState(State s) {
+        state = s;
     }
 
+    /**
+     * Gets the user's current state.
+     * @param s string version of user's current state (eg if user is in member state, s = "member")
+     * @return instantiation of the state class the user is in
+     */
     public State getState(String s){
         if(s.equals("member")){
             return member;
@@ -48,43 +72,98 @@ public class User {
         return null;
     }
 
+    /**
+     * Sets the user's location to the latitude and longitude provided in the parameters.
+     * @param latitude user's new latitude
+     * @param longitude user's new longitude
+     */
     public void setCoordinates(double latitude, double longitude) {
         lat = latitude;
         lon = longitude;
     }
 
+    /**
+     * Gets the user's current latitude
+     * @return user's latitude
+     */
     public double getLat() {
         return lat;
     }
 
+    /**
+     * Gets the user's current longitude
+     * @return user's longitude
+     */
     public double getLong() {
         return lon;
     }
 
-    public interface State{
+    /**
+     * Interface defining all possible user actions in a state. A user can join a group, create a group,
+     * start a chat with group members, leave a group, invite a user, or dissolve a group. What actions they
+     * can take depends on what state they are in.
+     */
+    public interface State {
+        /**
+         * Adds the user to the specified group
+         * @param group group to add user to
+         */
         void join_group(Group group);
-        Group create_group(User user);
-        void initiate_chat(User user);
-        void leave_group(Group group);
-        void invite_user(User user);
-        void dissolve_group(Group group);
 
+        /**
+         * Creates a new group, with the user as the creator
+         * @return newly created group
+         */
+        Group create_group();
+
+        /**
+         * Initiates a group chat.
+         * @param user
+         */
+        void initiate_chat(User user);
+
+        /**
+         * Removes the user from the specified group.
+         * @param group group to remove the user from
+         */
+        void leave_group(Group group);
+
+        /**
+         * Invite the specified user to the current group.
+         * @param user user to invite to group
+         */
+        void invite_user(User user);
+
+        /**
+         * Dissolve group created by the user.
+         * @param group group to dissolve
+         */
+        void dissolve_group(Group group);
     }
 
+    /**
+     * Free agent state. In this state, the user can only join a group or create a group.
+     */
     public class Free_agent implements State{
 
+        /**
+         * Adds a user to the group specified in the parameters.
+         * @param group group to add the user to.
+         */
         @Override
         public void join_group(Group group) {
             group.add_user(User.this);
             User.this.setState(User.this.getState("member"));
         }
 
+        /**
+         * Creates a group with the user as the creator.
+         * @return newly created group
+         */
         @Override
-        public Group create_group(User user) {
+        public Group create_group() {
             User.this.setState(User.this.getState("creator"));
             return new Group(User.this);
-
-
         }
 
         @Override
@@ -94,7 +173,6 @@ public class User {
 
         @Override
         public void leave_group(Group group) {
-
 
         }
 
@@ -110,7 +188,10 @@ public class User {
         }
     }
 
-    public class Member implements State{
+    /**
+     * Member state. In this state, a user can leave a group or initiate chat.
+     */
+    public class Member implements State {
 
         @Override
         public void join_group(Group group) {
@@ -118,15 +199,19 @@ public class User {
         }
 
         @Override
-        public Group create_group(User user) {
+        public Group create_group() {
             return null;
         }
 
         @Override
         public void initiate_chat(User user) {
-
+            // TODO?
         }
 
+        /**
+         * Removes the user from the specified group.
+         * @param group group to remove the user from
+         */
         @Override
         public void leave_group(Group group) {
             User.this.setState(User.this.getState("freeagent"));
@@ -144,6 +229,9 @@ public class User {
         }
     }
 
+    /**
+     * Creator state. In this state, the user can initiate chat, invite a user, or dissolve the group.
+     */
     public class Creator implements State{
 
         @Override
@@ -152,7 +240,7 @@ public class User {
         }
 
         @Override
-        public Group create_group(User user) {
+        public Group create_group() {
             return group;
         }
 
@@ -171,10 +259,15 @@ public class User {
 
         }
 
+        /**
+         * Dissolves the group the user created. Moves all members of the group into the free agent state
+         * @param group group to dissolve
+         */
         @Override
         public void dissolve_group(Group group) {
-            User.this.setState(User.this.getState("freeagent"));
-            group=null;
+            //User.this.setState(User.this.getState("freeagent"));
+            group.dissolveGroup();
+            group = null;
         }
     }
 }
